f797ffb1012b450b7ea5ed69d48a9336f7880e1a
dddooo9 <dlaehdus99@naver.com>
2022-02-14 15:23:10 +0900

    [CHORE] 데이터 바인딩 적용

--- app/src/main/java/com/simmons/testbed/MainActivity.kt
+++ app/src/main/java/com/simmons/testbed/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.simmons.testbed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.simmons.testbed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.btnSetBound.setOnClickListener {
+            startActivity(Intent(baseContext, CheckActivity::class.java))
+        }
     }
 }
